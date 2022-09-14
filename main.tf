ba3b428bc4378675041ca1a2e043cc633f4dcb13
thami618 <thamngcobo1998@gmail.com>
2022-09-15 00:43:39 +0200

    permissions

--- main.tf
+++ main.tf
@@ -17,15 +17,38 @@ resource "aws_iam_role" "tf-ppline-role" {
         "Service": "codepipeline.amazonaws.com"
       },
       "Effect": "Allow",
-      "Action": "codestar-connections:UseConnection",
-      "Resource": "insert ARN of the CodeStar connection here"
+      "Sid": ""
     }
   ]
 }
 EOF
-
+data "aws_iam_policy_document" "tf-cicd-pipeline-policies" {
+    statement{
+        sid = ""
+        actions = ["codestar-connections:UseConnection"]
+        resources = ["*"]
+        effect = "Allow"
+    }
+    statement{
+        sid = ""
+        actions = ["cloudwatch:*", "s3:*", "codebuild:*"]
+        resources = ["*"]
+        effect = "Allow"
+    }
+}
+resource "aws_iam_policy" "tf-cicd-pipeline-policy" {
+    name = "tf-cicd-pipeline-policy"
+    path = "/"
+    description = "Pipeline policy"
+    policy = data.aws_iam_policy_document.tf-cicd-pipeline-policies.json
 }
 
+resource "aws_iam_role_policy_attachment" "tf-cicd-pipeline-attachment" {
+    policy_arn = aws_iam_policy.tf-cicd-pipeline-policy.arn
+    role = aws_iam_role.tf-codepipeline-role.id
+}
+
+}
 data "aws_iam_policy_document" "tf-cicd-build-policies" {
     statement{
         sid = ""
@@ -34,6 +57,7 @@ data "aws_iam_policy_document" "tf-cicd-build-policies" {
         effect = "Allow"
     }
 }
+
 #Codebuild role
 resource "aws_iam_role" "tf-codebuild-role" {
   name = "tf-codebuild-role"
