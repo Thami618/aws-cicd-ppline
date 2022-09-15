1a43f683a33547f560b080af3e34425ea9db614a
thami618 <thamngcobo1998@gmail.com>
2022-09-15 11:20:11 +0200

    vpc

---
+++ vpc.tf
@@ -0,0 +1,24 @@
+resource "aws_vpc" "tf_demo" {
+    cidr_block = "10.0.0.0/16"
+    instance_tenancy = "default"
+    enable_dns_hostnames = false
+    tags = {
+      "Name" = "tf_demo"
+    }
+}
+resource "aws_subnet" "subnet-tf-public" {
+    vpc_id = aws_vpc.tf_demo.id
+    cidr_block = "10.0.1.0/24"
+    availability_zone = "us-east-1"
+    tags = {
+      "Name" = "subnet-tf-public"
+    }
+}
+resource "aws_subnet" "subnet-tf-private" {
+    vpc_id = aws_vpc.tf_demo.id
+    cidr_block = "10.0.2.0/24"
+    availability_zone = "us-east-1"
+    tags = {
+      "Name" = "subnet-tf-private"
+    }
+}
