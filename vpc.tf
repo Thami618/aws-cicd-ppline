27bd66e09b4b330944761d7aaa3a3242843057ef
thami618 <thamngcobo1998@gmail.com>
2022-09-15 11:23:05 +0200

    vpc, az

--- vpc.tf
+++ vpc.tf
@@ -9,7 +9,7 @@ resource "aws_vpc" "tf_demo" {
 resource "aws_subnet" "subnet-tf-public" {
     vpc_id = aws_vpc.tf_demo.id
     cidr_block = "10.0.1.0/24"
-    availability_zone = "us-east-1"
+    availability_zone = "us-east-1a"
     tags = {
       "Name" = "subnet-tf-public"
     }
@@ -17,7 +17,7 @@ resource "aws_subnet" "subnet-tf-public" {
 resource "aws_subnet" "subnet-tf-private" {
     vpc_id = aws_vpc.tf_demo.id
     cidr_block = "10.0.2.0/24"
-    availability_zone = "us-east-1"
+    availability_zone = "us-east-1b"
     tags = {
       "Name" = "subnet-tf-private"
     }
